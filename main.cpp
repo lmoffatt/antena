7710caa9373f17b072cda92a835327031ccba8a2
Luciano Moffatt <lmoffatt@gmail.com>
2019-11-01 17:02:34 -0300

    now it run samples

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ struct V{constexpr static auto  className=my_static_string("V");};
 struct GHz{constexpr static auto  className=my_static_string("GHz");};
 
 
-
 typedef p<u<ps,1>> ps_u;
 typedef p<u<V,1>> V_u;
 typedef p<u<GHz,1>> GHz_u;
@@ -66,6 +65,9 @@ constexpr const auto GHz_f=v(1E9,p<u<GHz,-1>,u<s,-1>>{});
 constexpr const auto ps_f=v(1E-12,p<u<s,1>,u<ps,-1>>{});
 
 
+template<class D> struct Distribution_of{constexpr static auto className=D::className+my_static_string("_Distribution");};
+template<class D> struct Values_of{constexpr static auto className=D::className+my_static_string("_Values");};
+
 struct prior{constexpr static auto className=my_static_string("prior");};
 
 struct model{constexpr static auto className=my_static_string("model");};
@@ -73,31 +75,70 @@ struct model{constexpr static auto className=my_static_string("model");};
 
 
 
+
+
 int main()
 {
+  auto myprior_values=vector_space{
+      x_i(mean<baseline>{},v(0.0,V_u{})),
+      x_i(stddev<baseline>{},v(1e-6,V_u{})),
+      x_i(mean<drift>{},v(0.0,V_u{}/ps_u{})),
+      x_i(stddev<drift>{},v(1e-7,V_u{}/ps_u{})),
+      x_i(mean<Log10_t<stddev<signal>>>{},logv<double,V_u>(-6.0,{1,V_u{}})),
+      x_i(stddev<Log10_t<stddev<signal>>>{},logv<double>(1.0)),
+      x_i(mean<ind<Log10_t<Amplitude>,0>>{},logv<double,V_u>(-5.0,{1,V_u{}})),
+      x_i(stddev<ind<Log10_t<Amplitude>,0>>{},logv<double>(1.0)),
+      x_i(mean<ind<Log10_t<Frecuency>,0>>{},logv<double,GHz_u>(std::log10(9),{1,GHz_u{}})),
+      x_i(stddev<ind<Log10_t<Frecuency>,0>>{},logv<double>(0.2)),
+      x_i(mean<ind<phase,0>>{},v(PI/4)),
+      x_i(stddev<ind<phase,0>>{},v(PI/4)),
+      x_i(mean<ind<Log10_t<tau>,0>>{},logv<double,ps_u>(std::log10(1000),{1,ps_u{}})),
+      x_i(stddev<ind<Log10_t<tau>,0>>{},logv<double>(1)),
+      x_i(mean<ind<Log10_t<Amplitude>,1>>{},logv<double,V_u>(-5.0,{1,V_u{}})),
+      x_i(stddev<ind<Log10_t<Amplitude>,1>>{},logv<double>(1.0)),
+      x_i(mean<ind<Log10_t<Frecuency>,1>>{},logv<double,GHz_u>(std::log10(9),{1,GHz_u{}})),
+      x_i(stddev<ind<Log10_t<Frecuency>,1>>{},logv<double>(0.2)),
+      x_i(mean<ind<phase,1>>{},v(PI/4)),
+      x_i(stddev<ind<phase,1>>{},v(PI/4)),
+      x_i(mean<ind<Log10_t<tau>,1>>{},logv<double,ps_u>(std::log10(1000),{1,ps_u{}})),
+      x_i(stddev<ind<Log10_t<tau>,1>>{},logv<double>(1))};
 
+  auto myprior_transf=quimulun{
+      F(stddev<signal>{},Log10_rev{},Log10_t<stddev<signal>>{}),
 
-  auto myprior=quimulun{prior{},
-    D(baseline{},Normal_Distribution{},mean<baseline>{},stddev<baseline>{}),
-        D(drift{},Normal_Distribution{},mean<baseline>{},stddev<baseline>{}),
-        D(ind<Amplitude,0>{},Normal_Distribution{},mean<ind<Amplitude,0>>{},stddev<ind<Amplitude,0>>{}),
-        D(ind<Frecuency,0>{},Normal_Distribution{},mean<ind<Frecuency,0>>{},stddev<ind<Frecuency,0>>{}),
+      F(ind<Amplitude,0>{},Log10_rev{},ind<Log10_t<Amplitude>,0>{}),
+      F(ind<Frecuency,0>{},Log10_rev{},ind<Log10_t<Frecuency>,0>{}),
+      F(ind<tau,0>{},Log10_rev{},ind<Log10_t<tau>,0>{}),
+
+      F(ind<Amplitude,1>{},Log10_rev{},ind<Log10_t<Amplitude>,1>{}),
+      F(ind<Frecuency,1>{},Log10_rev{},ind<Log10_t<Frecuency>,1>{}),
+      F(ind<tau,1>{},Log10_rev{},ind<Log10_t<tau>,1>{})
+
+  };
+
+  auto myprior_dist=quimulun{
+      D(baseline{},Normal_Distribution{},mean<baseline>{},stddev<baseline>{}),
+        D(drift{},Normal_Distribution{},mean<drift>{},stddev<drift>{}),
+        D(Log10_t<stddev<signal>>{},Normal_Distribution{},mean<Log10_t<stddev<signal>>>{},stddev<Log10_t<stddev<signal>>>{}),
+        D(ind<Log10_t<Amplitude>,0>{},Normal_Distribution{},mean<ind<Log10_t<Amplitude>,0>>{},stddev<ind<Log10_t<Amplitude>,0>>{}),
+        D(ind<Log10_t<Frecuency>,0>{},Normal_Distribution{},mean<ind<Log10_t<Frecuency>,0>>{},stddev<ind<Log10_t<Frecuency>,0>>{}),
         D(ind<phase,0>{},Normal_Distribution{},mean<ind<phase,0>>{},stddev<ind<phase,0>>{}),
-        D(ind<tau,0>{},Normal_Distribution{},mean<ind<tau,0>>{},stddev<ind<tau,0>>{}),
-        D(ind<Amplitude,1>{},Normal_Distribution{},mean<ind<Amplitude,1>>{},stddev<ind<Amplitude,1>>{}),
-        D(ind<Frecuency,1>{},Normal_Distribution{},mean<ind<Frecuency,1>>{},stddev<ind<Frecuency,1>>{}),
+        D(ind<Log10_t<tau>,0>{},Normal_Distribution{},mean<ind<Log10_t<tau>,0>>{},stddev<ind<Log10_t<tau>,0>>{}),
+        D(ind<Log10_t<Amplitude>,1>{},Normal_Distribution{},mean<ind<Log10_t<Amplitude>,1>>{},stddev<ind<Log10_t<Amplitude>,1>>{}),
+        D(ind<Log10_t<Frecuency>,1>{},Normal_Distribution{},mean<ind<Log10_t<Frecuency>,1>>{},stddev<ind<Log10_t<Frecuency>,1>>{}),
         D(ind<phase,1>{},Normal_Distribution{},mean<ind<phase,1>>{},stddev<ind<phase,1>>{}),
-        D(ind<tau,1>{},Normal_Distribution{},mean<ind<tau,1>>{},stddev<ind<tau,1>>{})};
+      D(ind<Log10_t<tau>,1>{},Normal_Distribution{},mean<ind<Log10_t<tau>,1>>{},stddev<ind<Log10_t<tau>,1>>{})};
 
   auto mymodel=
       quimulun{
-          model{},
           D(signal{},Normal_Distribution{},mean<signal>{},stddev<signal>{}),
           F(mean<signal>{},
               [](auto t, auto baseline_,auto drift_,
                  auto A0_, auto f0_, auto ph0_, auto tau0_,
-                 auto A1_, auto f1_, auto ph1_, auto tau1_ ){
-                return baseline_+drift_*t+
+                 auto A1_, auto f1_, auto ph1_, auto tau1_ )
+              {
+
+                    return baseline_+(drift_*t)+
                        A0_*exp(-t/tau0_)*cos(2*PI*f0_*GHz_f*t*ps_f+ph0_)+
                        A1_*exp(-t/tau1_)*cos(2*PI*f1_*GHz_f*t*ps_f+ph1_);},
               delay{},baseline{},drift{},
@@ -112,7 +153,22 @@ int main()
   std::ifstream fi(fname);
   from_DataTable(fi,data);
 
-  auto fname2="antena_data_2.txt";
+  auto totalmodel=mymodel+myprior_dist+myprior_transf+myprior_values;
+
+  auto data_prior=data;
+
+  std::random_device rd;
+  auto initseed = 0;
+  //rd();
+
+  std::mt19937 mt(initseed);
+
+  auto data2=data;
+  auto s=sample(totalmodel,std::move(data2),mt);
+
+  std::cerr<<s;
+
+/*  auto fname2="antena_data_2.txt";
   std::ofstream of(fname2);
   to_DataFrame(of,data);
 
@@ -124,10 +180,11 @@ int main()
   auto fname3="antena_data_3.txt";
   std::ofstream of3(fname3);
   to_DataTable(of3,data);
+*/
 
 
 
-  std::cerr<<data;
+//  std::cerr<<data;
 
 
 
